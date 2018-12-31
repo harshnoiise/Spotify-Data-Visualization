fbd66c97934e6c62b8c6198a4447bb06596c2fa1
Keaton Khonsari <keaton.khonsari@gmail.com>
2018-12-31 14:36:16 -0600

    Data Visualization

---
+++ credentials.py
@@ -0,0 +1,3 @@
+client_id='9ffbe8744f86444e97c5efaaef165563'
+client_secret='59da1dd1c03d4d9fa98fdc2cdc786eaf'
+redirect_uri='http://example.com/callback/'
--- danceability.py
+++ danceability.py
@@ -4,9 +4,22 @@ import pprint
 import spotipy.util as util
 from spotipy.oauth2 import SpotifyClientCredentials
 from credentials import *
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+
+import seaborn as sns
+import graphviz
+import pydotplus
+import io
+
+from scipy import misc
+from sklearn.metrics import accuracy_score
 
 cid = client_id
 secret = client_secret
@@ -23,8 +36,10 @@ if token:
 else:
     print("Can't get token for", username)
 
-
+bad_playlist = sp.user_playlist("127149407","4GXyhqA8RajKqAKqFymwiC")
 good_playlist = sp.user_playlist("127149407", "5IR1utIG0M7yoUegV7dSnc")
+
+# Retrieve song ID's of good_playlist
 good_tracks = good_playlist["tracks"]
 good_songs = good_tracks["items"]
 while good_tracks['next']:
@@ -35,13 +50,175 @@ good_ids = []
 for i in range(len(good_songs) - 500):
     good_ids.append(good_songs[i]['track']['id'])
 
+# Retrieve song ID's of bad_playlist
+bad_tracks = bad_playlist["tracks"]
+bad_songs = bad_tracks["items"]
+while bad_tracks['next']:
+    bad_tracks = sp.next(bad_tracks)
+    for item in bad_tracks["items"]:
+        bad_songs.append(item)
+bad_ids = []
+for i in range(len(bad_songs) - 500):
+    bad_ids.append(bad_songs[i]['track']['id'])
+
 features = []
+inSavedTracks = []
+
+# Retrieve audio features from good_playlist
+j = 0
 for i in range(0,len(good_ids),50):
     audio_features = sp.audio_features(good_ids[i:i+50])
     for track in audio_features:
         features.append(track)
+        track = good_songs[j]
+        j = j+1
+        features[-1]['trackPopularity'] = track['track']['popularity']
+        features[-1]['artistPopularity'] = sp.artist(track['track']['artists'][0]['id'])['popularity']
         features[-1]['target'] = 1
 
+# Retrieve audio features from bad_playlist
+j = 0
+for i in range(0,len(bad_ids),50):
+    audio_features = sp.audio_features(bad_ids[i:i+50])
+    for track in audio_features:
+        features.append(track)
+        track = good_songs[j]
+        j= j+1
+        features[-1]['trackPopularity'] = track['track']['popularity']
+        features[-1]['artistPopularity'] = sp.artist(track['track']['artists'][0]['id'])['popularity']
+        features[-1]['target'] = 0
+
+# Convert the training data into Pandas DataFrame
+trainingData = pd.DataFrame(features)
+train, test = train_test_split(trainingData, test_size = 0.15)
+print("Training size: {}, Test size: {}".format(len(train),len(test)))
+
+# Color palette for data visualization
+red_blue = ['#3498db', '#e74c3c']
+palette = sns.color_palette(red_blue)
+sns.set_palette(palette)
+sns.set_style('white')
+
+# Divide data into positive and negative categories for all audio features to be compared
+pos_tempo = trainingData[trainingData['target'] == 1]['tempo']
+neg_tempo = trainingData[trainingData['target'] == 0]['tempo']
+pos_dance = trainingData[trainingData['target'] == 1]['danceability']
+neg_dance = trainingData[trainingData['target'] == 0]['danceability']
+pos_duration = trainingData[trainingData['target'] == 1]['duration_ms']
+neg_duration = trainingData[trainingData['target'] == 0]['duration_ms']
+pos_loudness = trainingData[trainingData['target'] == 1]['loudness']
+neg_loudness = trainingData[trainingData['target'] == 0]['loudness']
+pos_speechiness = trainingData[trainingData['target'] == 1]['speechiness']
+neg_speechiness = trainingData[trainingData['target'] == 0]['speechiness']
+pos_valence = trainingData[trainingData['target'] == 1]['valence']
+neg_valence = trainingData[trainingData['target'] == 0]['valence']
+pos_energy = trainingData[trainingData['target'] == 1]['energy']
+neg_energy = trainingData[trainingData['target'] == 0]['energy']
+pos_acousticness = trainingData[trainingData['target'] == 1]['acousticness']
+neg_acousticness = trainingData[trainingData['target'] == 0]['acousticness']
+pos_key = trainingData[trainingData['target'] == 1]['key']
+neg_key = trainingData[trainingData['target'] == 0]['key']
+pos_instrumentalness = trainingData[trainingData['target'] == 1]['instrumentalness']
+neg_instrumentalness = trainingData[trainingData['target'] == 0]['instrumentalness']
+pos_popularity = trainingData[trainingData['target'] == 1]['trackPopularity']
+neg_popularity = trainingData[trainingData['target'] == 0]['trackPopularity']
+
+"""
+# Data visualization
+fig = plt.figure(figsize=(12,8))
+plt.title("Song Tempo Like / Dislike Distribution")
+pos_tempo.hist(alpha=0.7, bins=30, label='positive')
+neg_tempo.hist(alpha=0.7, bins=30, label='negative')
+plt.legend(loc='upper right')
+plt.show()
+"""
+
+fig2 = plt.figure(figsize=(15,15))
+
+# Danceability
+ax3 = fig2.add_subplot(331)
+ax3.set_xlabel('Danceability')
+ax3.set_ylabel('Count')
+ax3.set_title('Song Danceability Like Distribution')
+pos_dance.hist(alpha= 0.5, bins=30)
+ax4 = fig2.add_subplot(331)
+neg_dance.hist(alpha= 0.5, bins=30)
+
+# Duration_ms
+ax5 = fig2.add_subplot(332)
+ax5.set_xlabel('Duration')
+ax5.set_ylabel('Count')
+ax5.set_title('Song Duration Like Distribution')
+pos_duration.hist(alpha= 0.5, bins=30)
+ax6 = fig2.add_subplot(332)
+neg_duration.hist(alpha= 0.5, bins=30)
+
+# Loudness
+ax7 = fig2.add_subplot(333)
+ax7.set_xlabel('Loudness')
+ax7.set_ylabel('Count')
+ax7.set_title('Song Loudness Like Distribution')
+pos_loudness.hist(alpha= 0.5, bins=30)
+ax8 = fig2.add_subplot(333)
+neg_loudness.hist(alpha= 0.5, bins=30)
+
+# Speechiness
+ax9 = fig2.add_subplot(334)
+ax9.set_xlabel('Speechiness')
+ax9.set_ylabel('Count')
+ax9.set_title('Song Speechiness Like Distribution')
+pos_speechiness.hist(alpha= 0.5, bins=30)
+ax10 = fig2.add_subplot(334)
+neg_speechiness.hist(alpha= 0.5, bins=30)
+
+# Valence
+ax11 = fig2.add_subplot(335)
+ax11.set_xlabel('Valence')
+ax11.set_ylabel('Count')
+ax11.set_title('Song Valence Like Distribution')
+pos_valence.hist(alpha= 0.5, bins=30)
+ax12 = fig2.add_subplot(335)
+neg_valence.hist(alpha= 0.5, bins=30)
+
+# Energy
+ax13 = fig2.add_subplot(336)
+ax13.set_xlabel('Energy')
+ax13.set_ylabel('Count')
+ax13.set_title('Song Energy Like Distribution')
+pos_energy.hist(alpha= 0.5, bins=30)
+ax14 = fig2.add_subplot(336)
+neg_energy.hist(alpha= 0.5, bins=30)
+
+# Key
+ax15 = fig2.add_subplot(337)
+ax15.set_xlabel('Key')
+ax15.set_ylabel('Count')
+ax15.set_title('Song Key Like Distribution')
+pos_key.hist(alpha= 0.5, bins=30)
+ax16 = fig2.add_subplot(337)
+neg_key.hist(alpha= 0.5, bins=30)
+
+# Key
+ax15 = fig2.add_subplot(338)
+ax15.set_xlabel('Popularity')
+ax15.set_ylabel('Count')
+ax15.set_title('Popularity Distribution')
+pos_popularity.hist(alpha= 0.5, bins=30)
+ax16 = fig2.add_subplot(338)
+neg_popularity.hist(alpha= 0.5, bins=30)
+
+# Tempo
+ax1 = fig2.add_subplot(339)
+ax1.set_xlabel('Tempo')
+ax1.set_ylabel('Count')
+ax1.set_title('Song Tempo Like Distribution')
+pos_tempo.hist(alpha=0.5, bins=30)
+ax2 = fig2.add_subplot(339)
+neg_tempo.hist(alpha=0.5, bins=30)
+
+plt.tight_layout()
+plt.show()
+"""
 trainingData = pd.DataFrame(features)
 
 label = trainingData['danceability']
@@ -59,3 +236,4 @@ plt.ylabel('danceability', fontsize=10)
 plt.xticks(index, label, fontsize=5, rotation=30)
 plt.title('Danceability of songs in my Spotify Library')
 plt.show()
+"""
